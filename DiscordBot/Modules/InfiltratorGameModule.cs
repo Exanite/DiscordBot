97d208581f23f521e3a113238c8fb060ac9500df
Exanite <42710136+Exanite@users.noreply.github.com>
2020-10-21 15:13:37 -0700

    Add InfiltratorGameModule

---
+++ DiscordBot/Modules/InfiltratorGameModule.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace DiscordBot.Modules
+{
+    [Group("RPG")]
+    [Summary("Commands for the Infiltrator text-based RPG.")]
+    public class InfiltratorGameModule : ModuleBase<SocketCommandContext>
+    {
+        public static InfiltratorGame game;
+
+        [Command("StartGame")]
+        [Summary("Starts a new game of Infiltrator.")]
+        public async Task StartGame([Remainder] string name = "Infiltrator")
+        {
+            await Context.Channel.SendMessageAsync("Starting a new game of Infiltrator.");
+
+            game = new InfiltratorGame();
+            game.name = name;
+        }
+
+        [Command("Current")]
+        [Summary("Gets information about the current game.")]
+        public async Task GetCurrent()
+        {
+            if (game != null)
+            {
+                await Context.Channel.SendMessageAsync(
+                    $"Infiltrator:\n" +
+                    $"Game name: {game.name}\n" +
+                    $"Difficulty level: {game.difficultyLevel}\n" +
+                    $"Player count: {game.players.Count}");
+            }
+            else
+            {
+                await Context.Channel.SendMessageAsync("No games active.");
+            }
+        }
+    }
+
+    public class InfiltratorGame
+    {
+        public string name;
+
+        public List<Player> players = new List<Player>();
+        public int difficultyLevel = 0;
+    }
+
+    public class Player
+    {
+
+    }
+}
