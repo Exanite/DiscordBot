5a699cdf0bde0f3b0eec4d3ea8551566184c1baf
Exanite <42710136+Exanite@users.noreply.github.com>
2020-10-24 12:56:55 -0700

    Add Enemy.Factory.GetRandomName

--- DiscordBot/Infiltrator/Enemy.Factory.cs
+++ DiscordBot/Infiltrator/Enemy.Factory.cs
@@ -7,25 +7,82 @@ namespace DiscordBot.Infiltrator
     {
         public class Factory
         {
+            public static List<string> PrefixNames = new List<string>() // todo move this to a config file
+            {
+                "Blurred",
+                "Calculating",
+                "Chosen",
+                "Corrupted",
+                "Cruel",
+                "Dangerous",
+                "Deadly",
+                "Foreboding",
+                "Hostile",
+                "Lethal",
+                "Malicious",
+                "Merciless",
+                "Nihilist's",
+                "Planning",
+                "Precise",
+                "Resilient",
+                "Skilled",
+                "Spiteful",
+                "Swift",
+                "The Infiltrator's",
+                "Unreal",
+                "Vengeful",
+                "Vicious",
+            };
+
+            public static List<string> SuffixNames = new List<string>()
+            {
+                "of Accuracy",
+                "of Anger",
+                "of Annihilation",
+                "of Cunning",
+                "of Destruction",
+                "of Devouring",
+                "of Doom",
+                "of Efficiency",
+                "of Evasion",
+                "of Evil",
+                "of Exile",
+                "of Ferocity",
+                "of Finesse",
+                "of Intent",
+                "of Legerdemain",
+                "of Plunder",
+                "of Power",
+                "of Precision",
+                "of Rage",
+                "of Raiding",
+                "of Ruin",
+                "of Sortilege",
+                "of the Campaign",
+                "of the Elder",
+                "of the Path",
+            };
+
             public static List<string> EnemyNames = new List<string>()
             {
+                "Agent",
                 "Assassin",
-                "Shade",
-                "Hacker",
-                "Shroud",
-                "Spy",
-                "Virus",
-                "Slayer",
+                "Bug",
                 "Executioner",
+                "Exploiter",
+                "Glitch",
+                "Hacker",
                 "Hitman",
                 "Mercenary",
-                "Terminator",
-                "Secret Agent",
-                "Agent",
                 "Operative",
-                "Glitch",
-                "Bug",
-                "Exploiter",
+                "Rogue",
+                "Secret Agent",
+                "Shade",
+                "Shroud",
+                "Slayer",
+                "Spy",
+                "Terminator",
+                "Virus",
             };
 
             private readonly Random random = new Random();
@@ -34,7 +91,17 @@ namespace DiscordBot.Infiltrator
             {
                 int nameIndex = random.Next(0, EnemyNames.Count);
 
-                return new Enemy(EnemyNames[nameIndex], 10);
+                return new Enemy(GetRandomName(), 10);
+            }
+
+            public string GetRandomName()
+            {
+                int prefixIndex = random.Next(0, PrefixNames.Count);
+                int suffixIndex = random.Next(0, SuffixNames.Count);
+
+                int nameIndex = random.Next(0, EnemyNames.Count);
+
+                return $"{PrefixNames[prefixIndex]} {EnemyNames[nameIndex]} {SuffixNames[suffixIndex]}";
             }
         }
     }
