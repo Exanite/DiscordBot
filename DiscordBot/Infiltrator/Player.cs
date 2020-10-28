80600b5565a7203ce48996dce6d01b146b0f4a04
Exanite <42710136+Exanite@users.noreply.github.com>
2020-10-27 22:44:03 -0700

    Add Mention and FullUsername properties to Player

--- DiscordBot/Infiltrator/Player.cs
+++ DiscordBot/Infiltrator/Player.cs
@@ -9,15 +9,19 @@ namespace DiscordBot.Infiltrator
             Id = user.Id;
             Username = user.Username;
             Discriminator = user.Discriminator;
+            Mention = user.Mention;
         }
 
-        public ulong Id { get; set; }
-        public string Username { get; set; }
-        public string Discriminator { get; set; }
+        public ulong Id { get; }
+        public string Username { get; }
+        public string Discriminator { get; }
+        public string Mention { get; }
+
+        public string FullUsername => $"{Username}:{Discriminator}";
 
         public override string ToString()
         {
-            return $"{Username}:{Discriminator}";
+            return FullUsername;
         }
     }
 }
