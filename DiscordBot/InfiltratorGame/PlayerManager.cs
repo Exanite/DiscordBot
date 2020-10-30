2c57fb7e8cb743a01ef1c2642f4f15d607197ecc
Exanite <42710136+Exanite@users.noreply.github.com>
2020-10-29 17:54:54 -0700

    Add methods to PlayerManager

--- DiscordBot/InfiltratorGame/PlayerManager.cs
+++ DiscordBot/InfiltratorGame/PlayerManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Discord;
 using Newtonsoft.Json;
 
 namespace DiscordBot.InfiltratorGame
@@ -6,6 +7,26 @@ namespace DiscordBot.InfiltratorGame
     public class PlayerManager
     {
         [JsonProperty]
-        public Dictionary<ulong, Player> PlayersById = new Dictionary<ulong, Player>();
+        public Dictionary<ulong, Player> PlayersById { get; set; } = new Dictionary<ulong, Player>();
+
+        public Player GetFor(IUser user)
+        {
+            if (PlayersById.TryGetValue(user.Id, out Player player))
+            {
+                return player;
+            }
+            else
+            {
+                return CreateFor(user);
+            }
+        }
+
+        private Player CreateFor(IUser user)
+        {
+            var player = new Player(user);
+            PlayersById.Add(player.Id, player);
+
+            return player;
+        }
     }
 }
