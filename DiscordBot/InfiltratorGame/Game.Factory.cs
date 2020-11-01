69bf68661d7e300057c075f4fcd269f06e1dc6ad
Exanite <42710136+Exanite@users.noreply.github.com>
2020-10-31 17:21:19 -0700

    Add two overloads to Game.Factory.Create

--- DiscordBot/InfiltratorGame/Game.Factory.cs
+++ DiscordBot/InfiltratorGame/Game.Factory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Discord;
+using Discord.WebSocket;
 using DiscordBot.InfiltratorGame.Data;
 using DiscordBot.Services;
 
@@ -12,20 +13,34 @@ namespace DiscordBot.InfiltratorGame
             private readonly PlayerManager playerManager;
             private readonly EmbedHelper embedHelper;
             private readonly Enemy.Factory enemyFactory;
+            private readonly DiscordSocketClient client;
 
-            public Factory(PlayerManager playerManager, EmbedHelper embedHelper, Enemy.Factory enemyFactory)
+            public Factory(PlayerManager playerManager, EmbedHelper embedHelper, Enemy.Factory enemyFactory, DiscordSocketClient client)
             {
                 this.embedHelper = embedHelper;
                 this.enemyFactory = enemyFactory;
                 this.playerManager = playerManager;
+                this.client = client;
+            }
+
+            public Game Create(GameData data)
+            {
+                var guild = client.GetGuild(data.GuildId);
+                var channel = guild.GetTextChannel(data.ChannelId);
+
+                return Create(channel, data);
             }
 
             public Game Create(IGuild guild, IMessageChannel channel)
             {
-                var gameData = new GameData(guild.Id, channel.Id, DateTimeOffset.Now);
-                var game = new Game(playerManager, embedHelper, enemyFactory, channel, gameData);
+                var data = new GameData(guild.Id, channel.Id, DateTimeOffset.Now);
 
-                return game;
+                return Create(channel, data);
+            }
+
+            public Game Create(IMessageChannel channel, GameData data)
+            {
+                return new Game(playerManager, embedHelper, enemyFactory, channel, data);
             }
         }
     }
