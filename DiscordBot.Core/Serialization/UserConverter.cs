81b7716e614869be821e8502cc7aafe2c9bd839c
Exanite <42710136+Exanite@users.noreply.github.com>
2020-11-21 14:08:44 -0800

    Add UserConverter

---
+++ DiscordBot.Core/Serialization/UserConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Discord;
+using Discord.WebSocket;
+using Newtonsoft.Json;
+
+namespace DiscordBot.Core.Serialization
+{
+    public class UserConverter : JsonConverter<IUser>
+    {
+        private readonly DiscordSocketClient client;
+
+        public UserConverter(DiscordSocketClient client)
+        {
+            this.client = client;
+        }
+
+        public override IUser ReadJson(JsonReader reader, Type objectType, IUser existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            ulong id = (ulong)reader.Value;
+
+            return client.GetUser(id);
+        }
+
+        public override void WriteJson(JsonWriter writer, IUser value, JsonSerializer serializer)
+        {
+            writer.WriteValue(value.Id);
+        }
+    }
+}
