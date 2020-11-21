ff199d61f37d40e4b5eb12f2f54b0769253f6ba2
Exanite <42710136+Exanite@users.noreply.github.com>
2020-11-21 14:17:53 -0800

    Add SerializationAutofacModule

---
+++ DiscordBot/AutofacModules/Core/SerializationAutofacModule.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using DiscordBot.Core.Serialization;
+
+namespace DiscordBot.AutofacModules.Core
+{
+    public class SerializationAutofacModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<SerializationService>().SingleInstance();
+            builder.Register(x => x.Resolve<SerializationService>().Serializer).SingleInstance();
+
+            builder.RegisterType<UserConverter>().SingleInstance();
+        }
+    }
+}
