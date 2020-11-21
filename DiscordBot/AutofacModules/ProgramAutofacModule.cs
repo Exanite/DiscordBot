cc1f7eb5e5e2173d6048611b515066bd1f3eee7f
Exanite <42710136+Exanite@users.noreply.github.com>
2020-11-21 12:23:56 -0800

    Remove Assembly registration from DI container

--- DiscordBot/AutofacModules/ProgramAutofacModule.cs
+++ DiscordBot/AutofacModules/ProgramAutofacModule.cs
@@ -1,7 +1,5 @@
 ﻿using Autofac;
 using Autofac.Extensions.DependencyInjection;
-using DiscordBot.Core.Services;
-using DiscordBot.InfiltratorGame;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DiscordBot.AutofacModules
@@ -11,8 +9,6 @@ namespace DiscordBot.AutofacModules
         protected override void Load(ContainerBuilder builder)
         {
             builder.Populate(new ServiceCollection());
-            builder.Register(_ => typeof(DiscordBotService).Assembly).SingleInstance(); // todo find better way of registering assemblies
-            builder.Register(_ => typeof(GameManager).Assembly).SingleInstance();
         }
     }
 }
